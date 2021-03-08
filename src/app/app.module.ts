ed29824951e06bfd956d26fe26235086058338c4
CelioRochadaSilva <celioelea@hotmail.com>
2021-03-07 22:18:26 -0300

    adicionando funcionalidades de rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,37 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import {ReplacePipe} from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './erro-404/erro-404.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent, //ativando component e executando
     StarComponent, 
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([ // rotas//
+      {
+        path:'' , redirectTo:'courses' , pathMatch: 'full' // 1ª rota: base do app//
+      }, 
+      {
+        path:'courses' , component: CourseListComponent //pagina redirecionanto: 2ª rota
+      }, 
+      {
+        path:'**' , component: Error404Component
+
+      }
+
+    ])
     
   ],
   providers: [],
