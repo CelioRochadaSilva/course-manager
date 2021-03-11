fce18bbc6801466448739b8ff1282c302939f4d8
CelioRochadaSilva <celioelea@hotmail.com>
2021-03-10 22:06:16 -0300

    finalizando funcionalidade requisições http

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -6,21 +6,25 @@ import {Course} from './course';
     providedIn:'root'
 })
 export class CourseService{
-    private courseUrl: string = 'http://localhost:3100/api/courses';
+    private coursesUrl: string = 'http://localhost:3100/api/courses';
 
     constructor(private httpClient: HttpClient) {}   
     retrieveAll(): Observable<Course[]> {
-        return this.httpClient.get<Course[]>(this.courseUrl);
+        return this.httpClient.get<Course[]>(this.coursesUrl);
     }
     retrieveById(id: number): Observable<Course> { 
-        return this.httpClient.get<Course>(`${this.courseUrl}/${id}`);
+        return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
     }
     save(course:Course): Observable<Course>{
         if(course.id){
-            return this.httpClient.put<Course>(`${this.courseUrl}/${course.id}`, course);
+            return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course);
         }else{
-            return this.httpClient.post<Course>(`${this.courseUrl}`, course);
+            return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
         }
+        
+    }
+    deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
     }
 }
 var COURSES: Course[] = [
