be3924cfab6d51b4d15ecc82e603d33122b9904c
CelioRochadaSilva <celioelea@hotmail.com>
2021-03-11 22:16:44 -0300

    Segregando responsabilidades dos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,41 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import {ReplacePipe} from './pipe/replace.pipe';
+
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './erro-404/erro-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+
+import { CourseModule } from './courses/course.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent, //ativando component e executando
-    StarComponent, 
-    ReplacePipe,
+     //ativando component e executando
+     
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
-
+    Error404Component
+   
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([ // rotas//
-      {
-        path:'courses' , component: CourseListComponent //pagina redirecionanto: 1ª rota
-      }, 
       
-      {
-        path:'courses/info/:id', component:CourseInfoComponent 
-        //redirecionanto: 2ª rota
-      },
       {
         path:'' , redirectTo:'courses' , pathMatch:'full' // 3ª rota: base do app//
       }, 
